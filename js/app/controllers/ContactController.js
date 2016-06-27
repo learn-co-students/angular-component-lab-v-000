fbd1b8e3f44f91acafbcbd3c268b5145606f71e8
Tyler Smith <oats000@hotmail.com>
2016-06-27 00:24:16 +0000

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,23 +1,26 @@
-function ContactController() {
-	this.contacts = [{
-		name: 'Bill Gates',
-		email: 'bill@microsoft.com',
-		phone: '01234567890'
-	},{
-		name: 'Steve Jobs',
-		email: 'steve@apple.com',
-		phone: '12345678910'
-	},{
-		name: 'Joe Bloggs',
-		email: 'joe@bloggs.com',
-		phone: '13092019340'
-	},{
-		name: 'President Obama',
-		email: 'president@whitehouse.com',
-		phone: '75934988239'
-	}];
+var ContactController = {
+	controller: function ($scope){
+		this.contacts = [{
+			name: 'Bill Gates',
+			email: 'bill@microsoft.com',
+			phone: '01234567890'
+		},{
+			name: 'Steve Jobs',
+			email: 'steve@apple.com',
+			phone: '12345678910'
+		},{
+			name: 'Joe Bloggs',
+			email: 'joe@bloggs.com',
+			phone: '13092019340'
+		},{
+			name: 'President Obama',
+			email: 'president@whitehouse.com',
+			phone: '75934988239'
+		}];
+	},
+	controllerAs: 'contact'
 }
 
 angular
 	.module('app')
-	.controller('ContactController', ContactController);
+	.component('contactController', ContactController);
