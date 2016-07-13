6650521f0f2032d5f541169ec1f292fce90a7b3f
github username <github email address>
2016-07-13 18:53:23 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,26 +1,23 @@
-var ContactController = {
-	controller: function ($scope){
-		this.contacts = [{
-			name: 'Bill Gates',
-			email: 'bill@microsoft.com',
-			phone: '01234567890'
-		},{
-			name: 'Steve Jobs',
-			email: 'steve@apple.com',
-			phone: '12345678910'
-		},{
-			name: 'Joe Bloggs',
-			email: 'joe@bloggs.com',
-			phone: '13092019340'
-		},{
-			name: 'President Obama',
-			email: 'president@whitehouse.com',
-			phone: '75934988239'
-		}];
-	},
-	controllerAs: 'contact'
+function ContactController() {
+	this.contacts = [{
+		name: 'Bill Gates',
+		email: 'bill@microsoft.com',
+		phone: '01234567890'
+	},{
+		name: 'Steve Jobs',
+		email: 'steve@apple.com',
+		phone: '12345678910'
+	},{
+		name: 'Joe Bloggs',
+		email: 'joe@bloggs.com',
+		phone: '13092019340'
+	},{
+		name: 'President Obama',
+		email: 'president@whitehouse.com',
+		phone: '75934988239'
+	}];
 }
 
 angular
 	.module('app')
-	.component('contactController', ContactController);
+	.controller('ContactController', ContactController);
