220d786e4553d6cc83a9c2be18bd2ba40df29a26
corya0687 <corya0687@gmail.com>
2016-08-31 20:33:55 -0400

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,25 +1,25 @@
-function ContactCard() {
-	return {
-		scope: {
-			name: '=',
-			email: '=',
-			phone: '='
-		},
+var ContactCard = {
+		controller: 'ContactController',
+		controllerAs: 'contact',
 		template: [
 			'<div>',
 				'<h4>Contact Card</h4>',
 				'<label>Name:</label>',
-				'{{ name }}',
+				'{{ contact.name  }}',
 				'<label>Email:</label>',
-				'{{ email }}',
+				'{{ contact.email }}',
 				'<label>Phone:</label>',
-				'{{ phone }}',
+				'{{ contact.phone }}',
 			'</div>'
 		].join(''),
-		restrict: 'E'
+		restrict: 'E',
+		bindings: {
+			name: '=',
+			email: '=',
+			phone: '='
+		}
 	};
-}
 
 angular
 	.module('app')
-	.directive('contactCard', ContactCard);
+	.component('contactCard', ContactCard);
